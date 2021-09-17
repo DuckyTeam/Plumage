ea2b396ec0de89d198d8aff23ce87772ce37902c
Tanguy Vaessen <tanguy@ducky.eco>
2021-09-17 12:00:30 +0200

    chore(package): Init NPM and add eslint, typescript, prettier, commitizen, husky, lint-stage

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'import/no-unresolved': 0,
+    'no-shadow': 'off', // replaced by ts-eslint rule below
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  extends: [
+    '@ducky/eslint-config-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  globals: {},
+  overrides: [
+    {
+      files: ['*.spec.js'],
+      rules: {
+        'init-declarations': 0,
+        'max-lines-per-function': 0,
+        'max-statements': 0,
+        'max-lines': 0,
+        'no-sync': 0,
+      },
+    },
+  ],
+};
