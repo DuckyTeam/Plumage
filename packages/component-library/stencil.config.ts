97c1b80f6fbbfd2244970660b29445dc9515f8f2
Jake Hill <jakereh91@gmail.com>
2021-10-15 12:16:44 +0200

    feat(react): switch to mono-repo structure, include react-library

--- packages/component-library/stencil.config.ts
+++ packages/component-library/stencil.config.ts
@@ -1,9 +1,15 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'plmg',
   globalStyle: 'src/tokens/main.css',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@ducky/plumage',
+      proxiesFile: '../component-library-react/src/components.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
