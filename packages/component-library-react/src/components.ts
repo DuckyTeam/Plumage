c58fbdb7a0c163fa8ec6602b8e20c54813db6284
Tanguy Vaessen <tanguy@ducky.eco>
2021-10-19 09:29:05 +0200

    build(react): First react build after NPM LINK

---
+++ packages/component-library-react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ducky/plumage';
+
+import { defineCustomElements } from '@ducky/plumage/loader';
+
+defineCustomElements();
+export const PlmgP = /*@__PURE__*/createReactComponent<JSX.PlmgP, HTMLPlmgPElement>('plmg-p');
