7cbf08107f6e1c8f9d1d7463e475aa9fc50aff99
TanguyDucky <tanguy@ducky.eco>
2021-12-03 11:55:42 +0100

    WIP: resolve tokens chaining references

--- packages/component-library/src/tokens/config.js
+++ packages/component-library/src/tokens/config.js
@@ -3,19 +3,41 @@ const StyleDictionary = require('style-dictionary');
 StyleDictionary.registerTransform({
   name: 'size/px', // notice: the name is an override of an existing predefined method
   type: 'value',
-  matcher: function (prop) {
+  matcher: function (token) {
     /* supports both "pixel" and "pixels" */
     return (
-      prop &&
-      prop.original &&
-      prop.original.unit &&
-      prop.original.unit.startsWith('pixel')
+      token &&
+      token.original &&
+      token.original.unit &&
+      token.original.unit.startsWith('pixel')
     );
   },
-  transformer: function (prop) {
-    return `${prop.value}px`;
+  transformer: function (token) {
+    return `${token.value}px`;
   },
 });
+
+StyleDictionary.registerTransform({
+  name: `resolveChainingValues`,
+  type: `value`,
+  transitive: true,
+  matcher: function (token) {
+    return (
+        token &&
+        typeof token.value === 'string' &&
+        token.value.startsWith('$')
+    );
+  },
+  transformer: function (token) {
+    // token.value will be resolved and transformed at this point
+    let path = token.original.value.replace('$', '').split('.');
+    path.unshift(...['values', 'global']);
+    console.log('foo', token)
+    return 'foo';
+
+    // TODO resolve the path in design-tokens.json (ps: use token.filePath) recursively until there is no more '$'
+  }
+})
 
 module.exports = {
   source: ['./src/tokens/design-tokens.json'],
@@ -23,103 +45,103 @@ module.exports = {
     js: {
       transformGroup: 'js',
       buildPath: './src/tokens/dist/',
-      transforms: ['size/px', 'name/ti/camel'],
+      transforms: ['size/px', 'name/ti/camel', 'resolveChainingValues'],
       /* We split tokens into separate files - it will be easier to use them this way */
       files: [
         {
-          destination: 'sizes.css',
-          format: 'css/variables',
+          destination: 'sizes.scss',
+          format: 'scss/variables',
           filter: {
             type: 'sizing',
           },
         },
         {
-          destination: 'spacing.css',
-          format: 'css/variables',
+          destination: 'spacing.scss',
+          format: 'scss/variables',
           filter: {
             type: 'spacing',
           },
         },
         {
-          destination: 'colors.css',
-          format: 'css/variables',
+          destination: 'colors.scss',
+          format: 'scss/variables',
           filter: {
             type: 'color',
-          },
+          }
         },
         {
-          destination: 'typography.css',
-          format: 'css/variables',
+          destination: 'typography.scss',
+          format: 'scss/variables',
           filter: {
             type: 'typography',
           },
         },
         {
-          destination: 'borderRadius.css',
-          format: 'css/variables',
+          destination: 'borderRadius.scss',
+          format: 'scss/variables',
           filter: {
             type: 'borderRadius',
           },
         },
         {
-          destination: 'borderWidth.css',
-          format: 'css/variables',
+          destination: 'borderWidth.scss',
+          format: 'scss/variables',
           filter: {
             type: 'borderWidth',
           },
         },
         {
-          destination: 'boxShadow.css',
-          format: 'css/variables',
+          destination: 'boxShadow.scss',
+          format: 'scss/variables',
           filter: {
             type: 'boxShadow',
           },
         },
         {
-          destination: 'opacity.css',
-          format: 'css/variables',
+          destination: 'opacity.scss',
+          format: 'scss/variables',
           filter: {
             type: 'opacity',
           },
         },
         {
-          destination: 'fontFamilies.css',
-          format: 'css/variables',
+          destination: 'fontFamilies.scss',
+          format: 'scss/variables',
           filter: {
             type: 'fontFamilies',
           },
         },
         {
-          destination: 'lineHeights.css',
-          format: 'css/variables',
+          destination: 'lineHeights.scss',
+          format: 'scss/variables',
           filter: {
             type: 'lineHeights',
           },
         },
         {
-          destination: 'letterSpacing.css',
-          format: 'css/variables',
+          destination: 'letterSpacing.scss',
+          format: 'scss/variables',
           filter: {
             type: 'letterSpacing',
           },
         },
         {
-          destination: 'paragraphSpacing.css',
-          format: 'css/variables',
+          destination: 'paragraphSpacing.scss',
+          format: 'scss/variables',
           filter: {
             type: 'paragraphSpacing',
           },
         },
         {
-          destination: 'fontWeights.css',
-          format: 'css/variables',
+          destination: 'fontWeights.scss',
+          format: 'scss/variables',
           filter: {
             type: 'fontWeights',
           },
         },
         {
-          destination: 'fontSizes.css',
-          format: 'css/variables',
+          destination: 'fontSizes.scss',
+          format: 'scss/variables',
           filter: {
             type: 'fontSizes',
           },
