b4a4c5c7c7cc5a84fd681a5359f2641b069e6574
TanguyDucky <tanguy@ducky.eco>
2022-02-22 11:28:49 +0100

    Fix tests

--- packages/component-library/src/components/plmg-button/test/plmg-button.spec.tsx
+++ packages/component-library/src/components/plmg-button/test/plmg-button.spec.tsx
@@ -5,16 +5,12 @@ describe('plmg-button', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [Button],
-      html: `<plmg-button></plmg-button>`,
+      html: `<plmg-button>Content</plmg-button>`,
     });
 
     expect(page.root).toEqualHtml(`
       <plmg-button>
-        <mock:shadow-root>
-          <button class="filled medium primary">
-            <slot></slot>
-          </button>
-        </mock:shadow-root>
+          <button class="filled medium plmg-button primary" type="button">Content</button>
       </plmg-button>
     `);
   });
