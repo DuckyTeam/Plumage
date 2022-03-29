3e09315343981f172716c0f86ae5cd2ed95fea14
TanguyDucky <tanguy@ducky.eco>
2022-03-29 16:40:35 +0200

    test: Add test for all card feature

--- packages/component-library/src/components/plmg-card/test/plmg-card.spec.tsx
+++ packages/component-library/src/components/plmg-card/test/plmg-card.spec.tsx
@@ -11,12 +11,210 @@ describe('plmg-card', () => {
       <plmg-card>
         <mock:shadow-root>
         <div class="plmg-card">
-           <div class="content-area">
+           <div class="plmg-card-content-area">
              <slot name="slot-1"></slot>
              <slot name="slot-2"></slot>
            </div>
          </div>
         </mock:shadow-root>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with header text & icon, footer with button and slots 1 & 2', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card 
+    header-text="Hello world" 
+    bottom-action-text="Action" 
+    top-action-icon="home" 
+    top-action-label="card top action">
+        <div slot="slot-1">Slot 1</div>
+        <div slot="slot-2">Slot 2</div>
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card header-text="Hello world" bottom-action-text="Action" top-action-icon="home" top-action-label="card top action">
+        <mock:shadow-root>
+        <div class="plmg-card">
+            <div class="plmg-card-header">
+                <span>Hello world</span>
+                <plmg-button
+                  design="borderless"
+                  size="small"
+                  color="primary"
+                  icon-center="home"
+                  label="card top action"
+              />
+            </div>
+           <div class="plmg-card-content-area with-header with-footer">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+           <div class="plmg-card-footer">
+            <plmg-button
+              design="borderless"
+              size="small"
+              color="primary"
+            >
+              Action
+            </plmg-button>
+           </div>
+         </div>
+        </mock:shadow-root>
+        <div slot="slot-1">
+            Slot 1
+        </div>
+        <div slot="slot-2">
+          Slot 2
+        </div>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with header when a header text is given', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card 
+    header-text="Hello world">
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card header-text="Hello world">
+        <mock:shadow-root>
+        <div class="plmg-card">
+            <div class="plmg-card-header">
+                <span>Hello world</span>
+            </div>
+           <div class="plmg-card-content-area with-header">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+         </div>
+        </mock:shadow-root>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with header when a header icon is given', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card 
+    top-action-icon="home" 
+    top-action-label="card top action">
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card top-action-icon="home" top-action-label="card top action">
+        <mock:shadow-root>
+        <div class="plmg-card">
+            <div class="plmg-card-header">
+                <span></span>
+                <plmg-button
+                  design="borderless"
+                  size="small"
+                  color="primary"
+                  icon-center="home"
+                  label="card top action"
+              />
+            </div>
+           <div class="plmg-card-content-area with-header">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+         </div>
+        </mock:shadow-root>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with footer when bottom action text is given', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card 
+    bottom-action-text="Action" 
+    >
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card bottom-action-text="Action">
+        <mock:shadow-root>
+        <div class="plmg-card">
+           <div class="plmg-card-content-area with-footer">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+           <div class="plmg-card-footer">
+            <plmg-button
+              design="borderless"
+              size="small"
+              color="primary"
+            >
+              Action
+            </plmg-button>
+           </div>
+         </div>
+        </mock:shadow-root>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with slot 1, when given', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card >
+        <div slot="slot-1">Slot 1</div>
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card>
+        <mock:shadow-root>
+        <div class="plmg-card">
+           <div class="plmg-card-content-area">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+         </div>
+        </mock:shadow-root>
+        <div slot="slot-1">
+            Slot 1
+        </div>
+      </plmg-card>
+    `);
+  });
+
+  it('renders a card with slot 2, when given', async () => {
+    const page = await newSpecPage({
+      components: [Card],
+      html: `
+<plmg-card >
+        <div slot="slot-2">Slot 2</div>
+</plmg-card>
+`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-card>
+        <mock:shadow-root>
+        <div class="plmg-card">
+           <div class="plmg-card-content-area">
+             <slot name="slot-1"></slot>
+             <slot name="slot-2"></slot>
+           </div>
+         </div>
+        </mock:shadow-root>
+        <div slot="slot-2">
+            Slot 2
+        </div>
       </plmg-card>
     `);
   });
