43bee460aa874584a59281989c2d9665d7bf0e1d
James Marriott <jamesmarriott@Jamess-MacBook-Pro.local>
2022-05-03 10:34:05 +0200

    added div wrapper to storybook

--- packages/component-library/src/components/plmg-separator/plmg-separator.stories.js
+++ packages/component-library/src/components/plmg-separator/plmg-separator.stories.js
@@ -23,11 +23,15 @@ const SLOTS = [];
 
 const Template = (args) => {
   const el = document.createElement('plmg-separator');
+  // provides a fixed sized wrapper for the component to inherit height and width from
+  const wrapper = document.createElement('div');
+  wrapper.style.height = '200px';
+  wrapper.style.width = '200px';
   Utils.bindProps(el, PROPS, args);
   Utils.bindEvents(el, EVENTS, args);
   Utils.bindStyles(el, CSS_VARS, args);
-  Utils.bindSlots(el, SLOTS, args);
-  return el;
+  wrapper.appendChild(el);
+  return wrapper;
 };
 
 export const Primary = Template.bind({});
