eef8c8fead3d16911d6eca716dd9a728d15b3ac9
James Marriott <jamesmarriott@Jamess-MacBook-Pro.local>
2022-05-20 09:20:44 +0200

    e2e tests

--- packages/component-library/src/components/plmg-tooltip/readme.md
+++ packages/component-library/src/components/plmg-tooltip/readme.md
@@ -12,7 +12,7 @@
 | `arrowPosition`   | `arrow-position`    | Define tooltip arrow position  Allowed values:   - start,   - middle,   - end  Default: none                   | `"end" \| "middle" \| "start"`                     | `'middle'`  |
 | `arrowSide`       | `arrow-side`        | Define tooltip's arrow side  Allowed values:   - none,   - left,   - right,   - top,   - bottom  Default: none | `"bottom" \| "left" \| "none" \| "right" \| "top"` | `'none'`    |
 | `bgColor`         | `bg-color`          | Define tooltip's background color  Allowed values:   - neutral   - primary  Default: neutral                   | `"neutral" \| "primary"`                           | `'neutral'` |
-| `forceVisible`    | `force-visible`     | Force tooltip to remain visible  Used for demonstration / display purposes                                     | `boolean`                                          | `false`     |
+| `forceVisible`    | `force-visible`     | Force tooltip to remain visible for demostration pu  Disables event listener                                   | `boolean`                                          | `false`     |
 | `targetElementId` | `target-element-id` | ID for connected element. Required.                                                                            | `string`                                           | `undefined` |
 | `tooltipTitle`    | `tooltip-title`     | Tooltip Title Text  Any string  Required                                                                       | `string`                                           | `undefined` |
 
--- packages/component-library/src/components/plmg-tooltip/test/plmg-tooltip.e2e.ts
+++ packages/component-library/src/components/plmg-tooltip/test/plmg-tooltip.e2e.ts
@@ -1,54 +1,48 @@
-// import { newE2EPage } from '@stencil/core/testing';
-// import { AxePuppeteer } from '@axe-core/puppeteer';
-// import { Page } from 'puppeteer';
-
-// describe('plmg-tooltip', () => {
-//   it('renders', async () => {
-//     const page = await newE2EPage();
-//     await page.setContent('<plmg-tooltip></plmg-tooltip>');
-
-//     const element = await page.find('plmg-tooltip');
-//     expect(element).toHaveClass('hydrated');
-//   });
-
-//   describe('all possible variations', () => {
-//     it('are accessible', async () => {
-//       const page = await newE2EPage();
-
-//       let htmlContent = '';
-//       someControl.forEach((control) => {
-//         htmlContent += `
-//     <plmg-tooltip control="${control}">
-//   control="${control}"
-//     </plmg-tooltip>
-// <br/>
-//     `;
-//       });
-//       await page.setContent('<main>' + htmlContent + '</main>');
-
-//       const results = await new AxePuppeteer(page as unknown as Page)
-//         .disableRules([
-//           'document-title',
-//           'html-has-lang',
-//           'landmark-one-main',
-//           'page-has-heading-one',
-//         ])
-//         .analyze();
-
-//       expect(results.violations).toHaveLength(0);
-//     });
-//   });
-// });
-
 import { newE2EPage } from '@stencil/core/testing';
+import { AxePuppeteer } from '@axe-core/puppeteer';
+import { Page } from 'puppeteer';
 
 describe('plmg-tooltip', () => {
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent('<plmg-tooltip></plmg-tooltip>');
+
     const element = await page.find('plmg-tooltip');
+
     expect(element).toHaveClass('hydrated');
   });
-});
 
-// TO DO: Write proper tests
+  describe('all possible variations', () => {
+    it('are accessible', async () => {
+      const page = await newE2EPage();
+
+      const arrowSides = ['none', 'left', 'right', 'top', 'bottom'];
+      const arrowPositions = ['start', 'middle', 'end'];
+      const bgColors = ['neutral', 'primary'];
+
+      let htmlContent = '';
+      bgColors.forEach((bgcolor) => {
+        arrowSides.forEach((arrowSide) => {
+          arrowPositions.forEach((arrowPosition) => {
+            htmlContent += `
+            <plmg-tooltip bg-color="${bgcolor} arrow-side="${arrowSide} arrow-position="${arrowPosition}">
+            </plmg-tooltip>
+            `;
+          });
+        });
+      });
+      await page.setContent('<main>' + htmlContent + '</main>');
+
+      const results = await new AxePuppeteer(page as unknown as Page)
+        .disableRules([
+          'document-title',
+          'html-has-lang',
+          'landmark-one-main',
+          'page-has-heading-one',
+        ])
+        .analyze();
+
+      expect(results.violations).toHaveLength(0);
+    });
+  });
+});
