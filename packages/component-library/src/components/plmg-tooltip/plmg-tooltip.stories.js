60ce6cab6bff807797134f3d04c25d17357b1a57
TanguyDucky <tanguy@ducky.eco>
2022-06-02 11:10:58 +0200

    docs(tooltip): Adapt AllVariations

--- packages/component-library/src/components/plmg-tooltip/plmg-tooltip.stories.js
+++ packages/component-library/src/components/plmg-tooltip/plmg-tooltip.stories.js
@@ -1,9 +1,5 @@
 import * as Utils from '../../stories/StencilStorybookUtils';
-import {
-  backgroundColors,
-  positions,
-  arrowPositions,
-} from './plmg-tooltip.types';
+import { backgroundColors } from './plmg-tooltip.types';
 
 export default {
   title: 'Component/Tooltip',
@@ -162,27 +158,83 @@ export const TooltipTop = (args) => {
 };
 
 export const AllVariations = (args) => {
-  let htmlContent = '';
-  backgroundColors.forEach((backgroundColor) => {
-    positions.forEach((position) => {
-      arrowPositions.forEach((arrowPosition) => {
-        htmlContent += `
-        <plmg-tooltip content="${backgroundColor} ${arrowPosition} ${position}" background-color="${backgroundColor}" position="${position}" arrow-position="${arrowPosition}">
-        </plmg-tooltip>
-        `;
-      });
-    });
+  let htmlContent = ``;
+
+  // Arrow "none"
+  backgroundColors.forEach((bgColor) => {
+    htmlContent += `
+<div style="display: grid; grid-template-columns: 150px 150px 150px; gap: 10px; margin-top: 50px; margin-bottom: 50px;">
+    <span style="grid-column: 1/4; text-align: center;" id="${bgColor}-top-none">${bgColor} top none</span>
+    <plmg-tooltip target-element="${bgColor}-top-none" content="${bgColor} top none" background-color="${bgColor}" position="top" arrow-position="none">
+    </plmg-tooltip>
+    <span id="${bgColor}-left-none">${bgColor} left none</span>
+    <plmg-tooltip target-element="${bgColor}-left-none" content="${bgColor} left none" background-color="${bgColor}" position="left" arrow-position="none">
+    </plmg-tooltip>
+    <span></span>
+    <span style="text-align: end" id="${bgColor}-right-none">${bgColor} right none</span>
+    <plmg-tooltip target-element="${bgColor}-right-none" content="${bgColor} right none" background-color="${bgColor}" position="right" arrow-position="none">
+    </plmg-tooltip>
+    <span style="grid-column: 1/4; text-align: center;" id="${bgColor}-bottom-none">${bgColor} bottom none</span>
+    <plmg-tooltip target-element="${bgColor}-bottom-none" content="${bgColor} bottom none" background-color="${bgColor}" position="bottom" arrow-position="none">
+    </plmg-tooltip>
+</div>
+`;
   });
 
-  const el = document.createElement('div');
-  el.innerHTML = htmlContent.trim();
-  el.style.display = 'grid';
-  el.style.gridTemplateColumns = 'repeat(3, 1fr)';
-  el.style.gridTemplateRows = 'repeat(5, 1fr)';
-  el.style.gap = '50px';
-  el.style.justifyContent = 'space-between';
-  el.style['flex-wrap'] = 'wrap';
-  return el;
-};
+  // All arrow positions
+  backgroundColors.forEach((bgColor) => {
+    htmlContent += `
+<div style="display: grid; grid-template-columns: 150px 150px 150px; gap: 10px; margin-top: 50px; margin-bottom: 50px;">
+    <div style="grid-column: 1/4; display: grid; grid-template-columns: auto auto auto; gap: 10px; ">
+        <span id="${bgColor}-top-start">${bgColor} top start</span>
+        <plmg-tooltip target-element="${bgColor}-top-start" content="${bgColor} top start" background-color="${bgColor}" position="top" arrow-position="start">
+        </plmg-tooltip>
+        <span style="text-align: center" id="${bgColor}-top-middle">${bgColor} top middle</span>
+        <plmg-tooltip target-element="${bgColor}-top-middle" content="${bgColor} top middle" background-color="${bgColor}" position="top" arrow-position="middle">
+        </plmg-tooltip>
+        <span style="text-align: end" id="${bgColor}-top-end">${bgColor} top end</span>
+        <plmg-tooltip target-element="${bgColor}-top-end" content="${bgColor} top end" background-color="${bgColor}" position="top" arrow-position="end">
+        </plmg-tooltip>
+    </div>
+     
+    <span id="${bgColor}-left-start">${bgColor} left start</span><span></span>
+    <plmg-tooltip target-element="${bgColor}-left-start" content="${bgColor} left start Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="left" arrow-position="start">
+    </plmg-tooltip>
+    <span style="text-align: end" id="${bgColor}-right-start">${bgColor} right start</span>
+    <plmg-tooltip target-element="${bgColor}-right-start" content="${bgColor} right start Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="right" arrow-position="start">
+    </plmg-tooltip>
+    
+    
+    <span id="${bgColor}-left-middle">${bgColor} left middle</span><span></span>
+    <plmg-tooltip target-element="${bgColor}-left-middle" content="${bgColor} left middle Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="left" arrow-position="middle">
+    </plmg-tooltip>
+    <span style="text-align: end" id="${bgColor}-right-middle">${bgColor} right middle</span>
+    <plmg-tooltip target-element="${bgColor}-right-middle" content="${bgColor} right middle Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="right" arrow-position="middle">
+    </plmg-tooltip>
+    
+    <span id="${bgColor}-left-end">${bgColor} left end</span><span></span>
+    <plmg-tooltip target-element="${bgColor}-left-end" content="${bgColor} left end Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="left" arrow-position="end">
+    </plmg-tooltip>
+    <span style="text-align: end" id="${bgColor}-right-end">${bgColor} right end</span>
+    <plmg-tooltip target-element="${bgColor}-right-end" content="${bgColor} right end Lorem_Ipsum_for-longer_tooltip" background-color="${bgColor}" position="right" arrow-position="end">
+    </plmg-tooltip>
+    
+    <div style="grid-column: 1/4; display: grid; grid-template-columns: auto auto auto; gap: 10px; ">
+        <span id="${bgColor}-bottom-start">${bgColor} bottom start</span>
+        <plmg-tooltip target-element="${bgColor}-bottom-start" content="${bgColor} bottom start" background-color="${bgColor}" position="bottom" arrow-position="start">
+        </plmg-tooltip>
+        <span style="text-align: center" id="${bgColor}-bottom-middle">${bgColor} bottom middle</span>
+        <plmg-tooltip target-element="${bgColor}-bottom-middle" content="${bgColor} bottom middle" background-color="${bgColor}" position="bottom" arrow-position="middle">
+        </plmg-tooltip>
+        <span style="text-align: end" id="${bgColor}-bottom-end">${bgColor} bottom end</span>
+        <plmg-tooltip target-element="${bgColor}-bottom-end" content="${bgColor} bottom end" background-color="${bgColor}" position="bottom" arrow-position="end">
+        </plmg-tooltip>
+    </div>
+</div>
+    `;
+  });
 
+  return htmlContent;
+};
 AllVariations.storyName = 'All variations';
+AllVariations.parameters = { layout: 'centered' };
