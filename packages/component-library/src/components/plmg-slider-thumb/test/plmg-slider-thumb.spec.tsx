de04fdf8f155aee5583b4e4a998f75cedff1f7d4
jamesducky <james@ducky.eco>
2022-06-21 14:43:12 +0200

    generate readme and test files

---
+++ packages/component-library/src/components/plmg-slider-thumb/test/plmg-slider-thumb.spec.tsx
@@ -0,0 +1,9 @@
+// import { newSpecPage } from '@stencil/core/testing';
+// import { Slider } from '../plmg-slider';
+
+describe('plmg-slider', () => {
+  // TODO better tests like the one above
+  it('needs better tests', () => {
+    expect(true).toBeTruthy();
+  });
+});
