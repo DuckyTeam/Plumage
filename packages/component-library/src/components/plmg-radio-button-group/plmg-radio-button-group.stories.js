9aca0a42c3e1f3e02beff2e1d78c986d6997cf5c
TanguyDucky <tanguy@ducky.eco>
2022-06-24 11:17:51 +0200

    Add a button to reveal error message on RadioButtonGroup error prop

--- packages/component-library/src/components/plmg-radio-button-group/plmg-radio-button-group.stories.js
+++ packages/component-library/src/components/plmg-radio-button-group/plmg-radio-button-group.stories.js
@@ -73,9 +73,7 @@ AllSizes.storyName = 'All sizes';
 export const AllRequired = (args) => {
   const htmlContent = [true, false]
     .map(
-      (
-        required
-      ) => `<plmg-radio-button-group size="medium" required="${required}" label="${
+      (required) => `<plmg-radio-button-group required="${required}" label="${
         required ? 'Required' : 'Not Required'
       }" name="${required ? 'Required' : 'Not Required'}" values='[
         "some",
@@ -98,23 +96,24 @@ AllRequired.storyName = 'All required';
 
 export const AllErrors = (args) => {
   const htmlContent = [
-    `<plmg-radio-button-group size="medium" error-message="Uh Oh! Something awful has happened!!!" label="With Error" name="error" values='[
+    `<plmg-radio-button-group error-message="Uh Oh! Something awful has happened!!!" required label="With Error" name="error" values='[
     "some",
     "error-riddled",
     "radio",
     "buttons"
   ]'></plmg-radio-button-group>`,
-    `<plmg-radio-button-group size="medium" label="Without Error" name="no error" values='[
+    `<plmg-radio-button-group  label="Without Error" name="no error" values='[
     "some",
     "error-free",
     "radio",
     "buttons"
   ]'></plmg-radio-button-group>`,
+    `<plmg-button type="submit" style="width: 100%; margin-top: 25px;">Click to reveal the error</plmg-button>`,
   ]
     .join('')
     .trim();
 
-  const el = document.createElement('div');
+  const el = document.createElement('form');
   el.innerHTML = htmlContent;
   el.style.display = 'flex';
   el.style.justifyContent = 'space-between';
