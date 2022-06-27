1bb63ede90064438643406e968c488014138c0a8
jamesducky <james@ducky.eco>
2022-06-27 17:27:43 +0200

    remove deleted plmgslideremake component docs

--- packages/component-library/src/components.d.ts
+++ packages/component-library/src/components.d.ts
@@ -191,36 +191,6 @@ export namespace Components {
          */
         "thumbLabel": boolean;
     }
-    interface PlmgSliderRemake {
-        /**
-          * Define the default value.  Sets the starting value for the slider.  Allowed: Any number  When the default value is outside of the min and max values or undefined, default value is set to the min value.
-         */
-        "defaultValue": number;
-        /**
-          * Define an id attribute for the input  Any string
-         */
-        "inputId": string;
-        /**
-          * Define if marks and marks labels are visible  Default: true
-         */
-        "marks": boolean;
-        /**
-          * Define a name for the slider  Any string
-         */
-        "name": string;
-        /**
-          * Define the range of values  Must be a list of values with at least two items  First and last items set min and max values  Additional values set mark additional labels  Required
-         */
-        "rangeValues": Array<number>;
-        /**
-          * Define step  Slider's value will increase or decrease in stepValue  Allowed values - Any number  When step is not provided step, step is set to 1% of the range
-         */
-        "step": number;
-        /**
-          * Define thumb label visibility  Allowed values  - true  - false  Default: true
-         */
-        "thumbLabel": boolean;
-    }
     interface PlmgSvgIcon {
         /**
           * Define icon's color.  Can be any valid CSS color value.  By default, the icon will have the same color as the parent's element.
@@ -297,12 +267,6 @@ declare global {
         prototype: HTMLPlmgSliderElement;
         new (): HTMLPlmgSliderElement;
     };
-    interface HTMLPlmgSliderRemakeElement extends Components.PlmgSliderRemake, HTMLStencilElement {
-    }
-    var HTMLPlmgSliderRemakeElement: {
-        prototype: HTMLPlmgSliderRemakeElement;
-        new (): HTMLPlmgSliderRemakeElement;
-    };
     interface HTMLPlmgSvgIconElement extends Components.PlmgSvgIcon, HTMLStencilElement {
     }
     var HTMLPlmgSvgIconElement: {
@@ -318,7 +282,6 @@ declare global {
         "plmg-sidebar": HTMLPlmgSidebarElement;
         "plmg-sidebar-item": HTMLPlmgSidebarItemElement;
         "plmg-slider": HTMLPlmgSliderElement;
-        "plmg-slider-remake": HTMLPlmgSliderRemakeElement;
         "plmg-svg-icon": HTMLPlmgSvgIconElement;
     }
 }
@@ -511,36 +474,6 @@ declare namespace LocalJSX {
          */
         "thumbLabel"?: boolean;
     }
-    interface PlmgSliderRemake {
-        /**
-          * Define the default value.  Sets the starting value for the slider.  Allowed: Any number  When the default value is outside of the min and max values or undefined, default value is set to the min value.
-         */
-        "defaultValue"?: number;
-        /**
-          * Define an id attribute for the input  Any string
-         */
-        "inputId"?: string;
-        /**
-          * Define if marks and marks labels are visible  Default: true
-         */
-        "marks"?: boolean;
-        /**
-          * Define a name for the slider  Any string
-         */
-        "name"?: string;
-        /**
-          * Define the range of values  Must be a list of values with at least two items  First and last items set min and max values  Additional values set mark additional labels  Required
-         */
-        "rangeValues"?: Array<number>;
-        /**
-          * Define step  Slider's value will increase or decrease in stepValue  Allowed values - Any number  When step is not provided step, step is set to 1% of the range
-         */
-        "step"?: number;
-        /**
-          * Define thumb label visibility  Allowed values  - true  - false  Default: true
-         */
-        "thumbLabel"?: boolean;
-    }
     interface PlmgSvgIcon {
         /**
           * Define icon's color.  Can be any valid CSS color value.  By default, the icon will have the same color as the parent's element.
@@ -564,7 +497,6 @@ declare namespace LocalJSX {
         "plmg-sidebar": PlmgSidebar;
         "plmg-sidebar-item": PlmgSidebarItem;
         "plmg-slider": PlmgSlider;
-        "plmg-slider-remake": PlmgSliderRemake;
         "plmg-svg-icon": PlmgSvgIcon;
     }
 }
@@ -580,7 +512,6 @@ declare module "@stencil/core" {
             "plmg-sidebar": LocalJSX.PlmgSidebar & JSXBase.HTMLAttributes<HTMLPlmgSidebarElement>;
             "plmg-sidebar-item": LocalJSX.PlmgSidebarItem & JSXBase.HTMLAttributes<HTMLPlmgSidebarItemElement>;
             "plmg-slider": LocalJSX.PlmgSlider & JSXBase.HTMLAttributes<HTMLPlmgSliderElement>;
-            "plmg-slider-remake": LocalJSX.PlmgSliderRemake & JSXBase.HTMLAttributes<HTMLPlmgSliderRemakeElement>;
             "plmg-svg-icon": LocalJSX.PlmgSvgIcon & JSXBase.HTMLAttributes<HTMLPlmgSvgIconElement>;
         }
     }
