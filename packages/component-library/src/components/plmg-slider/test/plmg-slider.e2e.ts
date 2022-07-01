9c0495cb06932fac7815ee6d0a9adb031c48e2b0
jamesducky <james@ducky.eco>
2022-07-01 11:01:54 +0200

    add tests - breaking

--- packages/component-library/src/components/plmg-slider/test/plmg-slider.e2e.ts
+++ packages/component-library/src/components/plmg-slider/test/plmg-slider.e2e.ts
@@ -1,41 +1,45 @@
-// import { newE2EPage } from '@stencil/core/testing';
-// import { AxePuppeteer } from '@axe-core/puppeteer';
-// import { Page } from 'puppeteer';
+import { newE2EPage } from '@stencil/core/testing';
+import { AxePuppeteer } from '@axe-core/puppeteer';
+import { Page } from 'puppeteer';
 
-// describe('plmg-slider-remake', () => {
-//   it('renders', async () => {
-//     const page = await newE2EPage();
-//     await page.setContent('<plmg-slider-remake></plmg-slider-remake>');
+describe('plmg-slider', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<plmg-slider></plmg-slider');
 
-//     const element = await page.find('plmg-slider-remake');
-//     expect(element).toHaveClass('hydrated');
-//   });
+    const element = await page.find('plmg-slider');
+    expect(element).toHaveClass('hydrated');
+  });
 
-//   describe('all possible variations', () => {
-//     it('are accessible', async () => {
-//       const page = await newE2EPage();
+  describe('all possible variations', () => {
+    it('are accessible', async () => {
+      const page = await newE2EPage();
 
-//       let htmlContent = '';
-//       someControl.forEach((control) => {
-//                 htmlContent += `
-//     <plmg-slider-remake control="${control}">
-//   control="${control}"
-//     </plmg-slider-remake>
-// <br/>
-//     `;
-//       });
-//       await page.setContent('<main>' + htmlContent + '</main>');
+      const marks = [true, false];
+      const thumbLabel = [true, false];
 
-//       const results = await new AxePuppeteer(page as unknown as Page)
-//         .disableRules([
-//           'document-title',
-//           'html-has-lang',
-//           'landmark-one-main',
-//           'page-has-heading-one',
-//         ])
-//         .analyze();
+      let htmlContent = '';
+      marks.forEach((mark) => {
+        thumbLabel.forEach((thumb) => {
+          htmlContent += `
+            <plmg-slider name='slider' range='0, 100' marks=${mark} thumb-label=${thumb}>
+            </plmg-slider>
+            <br/>
+            `;
+        });
+      });
+      await page.setContent('<main>' + htmlContent + '</main>');
 
-//       expect(results.violations).toHaveLength(0);
-//     });
-//   });
-// });
+      const results = await new AxePuppeteer(page as unknown as Page)
+        .disableRules([
+          'document-title',
+          'html-has-lang',
+          'landmark-one-main',
+          'page-has-heading-one',
+        ])
+        .analyze();
+
+      expect(results.violations).toHaveLength(0);
+    });
+  });
+});
--- packages/component-library/src/components/plmg-slider/test/plmg-slider.spec.tsx
+++ packages/component-library/src/components/plmg-slider/test/plmg-slider.spec.tsx
@@ -1,18 +1,27 @@
-// import { newSpecPage } from '@stencil/core/testing';
-// import { SliderRemake } from '../plmg-slider-remake';
+import { newSpecPage } from '@stencil/core/testing';
+import { Slider } from '../plmg-slider';
 
-// describe('plmg-slider-remake', () => {
-//   it('renders', async () => {
-//     const page = await newSpecPage({
-//       components: [SliderRemake],
-//       html: `<plmg-slider-remake></plmg-slider-remake>`,
-//     });
-//     expect(page.root).toEqualHtml(`
-//       <plmg-slider-remake>
-//         <mock:shadow-root>
-//           <slot></slot>
-//         </mock:shadow-root>
-//       </plmg-slider-remake>
-//     `);
-//   });
-// });
+describe('plmg-slider', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [Slider],
+      html: `<plmg-slider default-value="25" step="1" name="range-slider" marks="false" thumb-label="true" range-values="0,5,10,20,30,50" value="25"></plmg-slider>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <plmg-slider default-value="25" marks="false" name="range-slider" range-values="0,5,10,20,30,50" step="1" thumb-label="true" value="0">
+        <mock:shadow-root>
+          <div class="plmg-component-container">
+          <div class="plmg-slider-container">
+          0
+          </div>
+          <div class="plmg-slider-input-field-container">
+            <label htmlfor="slider-input">
+            <input aria-valuemax="0" aria-valuemin="0" aria-valuenow="0" max="0" min="0" name="slider-input" step="1" type="number" value="0">
+            </label>
+          </div>
+        </div>
+        </mock:shadow-root>
+      </plmg-slider>
+    `);
+  });
+});
