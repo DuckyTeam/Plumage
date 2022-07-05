ce623540b43e6a272c7c32c43ce808a4d1ce2f0b
jamesducky <james@ducky.eco>
2022-07-05 16:53:18 +0200

    rename labeltext to label

--- packages/component-library/src/components/plmg-text-input/test/plmg-text-input.spec.tsx
+++ packages/component-library/src/components/plmg-text-input/test/plmg-text-input.spec.tsx
@@ -5,14 +5,14 @@ describe('plmg-text-input', () => {
   it('renders default', async () => {
     const page = await newSpecPage({
       components: [TextInput],
-      html: `<plmg-text-input label-text="Text Input"></plmg-text-input>`,
+      html: `<plmg-text-input label="Text Input"></plmg-text-input>`,
     });
     expect(page.root).toEqualHtml(`
 
-    <plmg-text-input label-text="Text Input">
-    <label aria-hidden="false" class="label medium" htmlfor="text-input">Text Input</label>
+    <plmg-text-input label="Text Input">
+    <label class="label medium" htmlfor="text-input" aria-label="Text Input">Text Input</label>
       <div class="plmg-text-input-field-wrapper" tabindex="0">
-        <input class="medium" autocomplete="off" name="Text Input" id="text-input" type="text">
+        <input class="medium" id="text-input" type="text" name="Text Input">
         </div>
     </plmg-text-input>
     `);
