a0f9ca53c2528dfdfc1b083b626d7d9010a27d09
jamesducky <james@ducky.eco>
2022-07-06 10:47:08 +0200

    react app changes

--- examples/react-app/src/TextForm.tsx
+++ examples/react-app/src/TextForm.tsx
@@ -1,32 +1,30 @@
-// import { useState } from "react";
+import { useState } from 'react';
 import { PlmgTextInput } from '@ducky/plumage-react';
 
 export default function TextForm() {
-  // const [sliders, setSlider] = useState({
-  //   stepped: 0,
-  //   decimal: 0.1,
-  //   large: 100000,
-  //   marked: 0,
-  // });
+  const [input, setInputs] = useState({
+    text: '',
+  });
 
-  // const handleSlider = (event: any) => {
-  //   const { name } = event.target;
-  //   const { value } = event.detail;
-  //   if (sliders[name] !== value) {
-  //     setSlider((prevState) => ({
-  //       ...prevState,
-  //       [name]: value,
-  //     }));
-  //   }
-  //   event.preventDefault();
-  //   event.stopPropagation();
-  // };
-
-  const handleSubmit = () => {
-    alert(`Submitted Slider Values
-      Stepped Slider: ${text - inputs}
-    `);
+  const handleChange = (event: any) => {
+    console.log(event);
+    const { label } = event.target;
+    const { value } = event.detail;
+    if (input[label] !== value) {
+      setInputs((prevState) => ({
+        ...prevState,
+        [label]: value,
+      }));
+    }
+    event.preventDefault();
+    event.stopPropagation();
   };
+
+  // const handleSubmit = () => {
+  //   alert(`Submitted Slider Values
+  //     Stepped Slider: ${text - inputs}
+  //   `);
+  // };
 
   return (
     <div>
@@ -34,7 +32,11 @@ export default function TextForm() {
       <div style={{ border: '4px dotted blue', padding: '0px 5px 5px 5px' }}>
         <h2>Text Input Form</h2>
         <form name={'text-inputs'}>
-          <PlmgTextInput label={'Text Input'} />
+          <PlmgTextInput
+            label={'text'}
+            onValueUpdated={(event) => handleChange(event)}
+          />
+          <p>{input.text}</p>
         </form>
       </div>
       <br />
