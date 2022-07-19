0818af671750fd65786d8ecd0401db24f0767dea
jamesducky <james@ducky.eco>
2022-07-19 10:53:22 +0100

    added more explicit unit test

--- packages/component-library/src/components/plmg-slider/test/plmg-slider.spec.tsx
+++ packages/component-library/src/components/plmg-slider/test/plmg-slider.spec.tsx
@@ -2,20 +2,41 @@ import { newSpecPage } from '@stencil/core/testing';
 import { Slider } from '../plmg-slider';
 
 describe('plmg-slider', () => {
-  it('renders', async () => {
+  it('builds', () => {
+    expect(new Slider()).toBeTruthy();
+  });
+
+  it('the name prop has a value', async () => {
     const page = await newSpecPage({
       components: [Slider],
-      html: `<plmg-slider default-value="25" step="1" name="Range Slider" marks="false" thumb-label="true" range-values="0,5,10,20,30,50" value="25"></plmg-slider>`,
+      html: `<div></div>`,
     });
+    let component = page.doc.createElement('plmg-slider');
+    (component as any).name = 'My Slider';
+    page.root.appendChild(component);
+    await page.waitForChanges();
+    expect(page.rootInstance.name).toBe('My Slider');
+  });
+
+  it('initially renders the component without the range input', async () => {
+    const page = await newSpecPage({
+      components: [Slider],
+      html: `<div></div>`,
+    });
+    let component = page.doc.createElement('plmg-slider');
+    (component as any).name = 'Range Slider';
+    (component as any).marks = 'false';
+    page.root.appendChild(component);
+    await page.waitForChanges();
     expect(page.root).toEqualHtml(`
-      <plmg-slider default-value="25" marks="false" name="Range Slider" range-values="0,5,10,20,30,50" step="1" thumb-label="true" value="0">
+      <plmg-slider>
         <mock:shadow-root>
           <div class="plmg-component-container">
           <div class="plmg-slider-container">
           </div>
           <div class="plmg-slider-input-field-container" tabindex="0">
             <label aria-label="Range Slider" htmlfor="range-slider-number-input">
-            <input aria-valuemax="0" aria-valuemin="0" aria-valuenow="0" max="0" min="0" id="range-slider-number-input" name="Range Slider" step="1" type="number" value="0">
+            <input id="range-slider-number-input" name="Range Slider" type="number">
             </label>
           </div>
         </div>
