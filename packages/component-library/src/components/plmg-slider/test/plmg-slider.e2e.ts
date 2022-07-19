a27f4c8ed91c8f2bd516ad0d7d093d9927e2ce37
nathan-bird <nathan2484@hotmail.co.uk>
2022-07-20 01:59:36 +0200

    refactor(e2e):Clean up e2e test code

--- packages/component-library/src/components/plmg-slider/test/plmg-slider.e2e.ts
+++ packages/component-library/src/components/plmg-slider/test/plmg-slider.e2e.ts
@@ -11,64 +11,25 @@ describe('plmg-slider', () => {
 
     const element = await page.find('plmg-slider');
 
-    // console.log(element);
     expect(element).toHaveClass('hydrated');
   });
 
-  describe('default', () => {
-    it('is accessible', async () => {
-      const page = await newE2EPage();
+  it('default slider is accessible', async () => {
+    const page = await newE2EPage();
 
-      let htmlContent = '';
-      htmlContent += `
-        <plmg-slider name='Range Slider' range='0, 100'>
-        </plmg-slider>
-        `;
+    const htmlContent = `<plmg-slider name='Range Slider' range='0, 100'></plmg-slider>`;
 
-      await page.setContent('<main>' + htmlContent + '</main>');
+    await page.setContent('<main>' + htmlContent + '</main>');
 
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
 
-      expect(results.violations).toHaveLength(0);
-    });
+    expect(results.violations).toHaveLength(0);
   });
 });
-
-// describe('all possible variations', () => {
-//   it('are accessible', async () => {
-//     const page = await newE2EPage();
-
-//     const marks = [true, false];
-//     const thumbLabel = [true, false];
-
-//     let htmlContent = '';
-//     marks.forEach((mark) => {
-//       thumbLabel.forEach((thumb) => {
-//         htmlContent += `
-//           <plmg-slider name='slider ${mark} ${thumb}' range='0, 100' marks=${mark} thumb-label=${thumb}>
-//           </plmg-slider>
-//           <br/>
-//           `;
-//       });
-//     });
-//     await page.setContent('<main>' + htmlContent + '</main>');
-
-//     const results = await new AxePuppeteer(page as unknown as Page)
-//       .disableRules([
-//         'document-title',
-//         'html-has-lang',
-//         'landmark-one-main',
-//         'page-has-heading-one',
-//       ])
-//       .analyze();
-
-//     expect(results.violations).toHaveLength(0);
-//   });
-// });
