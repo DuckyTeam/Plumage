19daab631e8ae90b9d4a0802f5d803893142ac5a
nathan-bird <nathan2484@hotmail.co.uk>
2022-07-20 01:05:25 +0200

    refactor(e2e):format e2e tests and remove extra test cases

--- packages/component-library/src/components/plmg-text-input/test/plmg-text-input.e2e.ts
+++ packages/component-library/src/components/plmg-text-input/test/plmg-text-input.e2e.ts
@@ -10,146 +10,126 @@ describe('plmg-text-input', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  describe('all sizes', () => {
-    it('are accessible', async () => {
-      const page = await newE2EPage();
+  it('all sizes are accessible', async () => {
+    const page = await newE2EPage();
 
-      const sizes = ['medium', 'large'];
+    const sizes = ['medium', 'large'];
 
-      let htmlContent = '';
-      sizes.forEach((sizeControl) => {
-        htmlContent += `
-    <plmg-text-input label='label name ${sizeControl}' size='${sizeControl}'>
-    </plmg-text-input>
-    `;
-      });
-      await page.setContent('<main>' + htmlContent + '</main>');
-
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
-
-      expect(results.violations).toHaveLength(0);
+    let htmlContent = '';
+    sizes.forEach((sizeControl) => {
+      htmlContent += `
+  <plmg-text-input label='label name ${sizeControl}' size=${sizeControl}>
+  </plmg-text-input>
+  `;
     });
+    await page.setContent('<main>' + htmlContent + '</main>');
+
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
+
+    expect(results.violations).toHaveLength(0);
   });
 
-  describe('all error states', () => {
-    it('are accessible', async () => {
-      const page = await newE2EPage();
+  it('error state is accessible', async () => {
+    const page = await newE2EPage();
 
-      const errors = ['error', undefined];
+    const htmlContent = `
+      <plmg-text-input label='error' error-message='error'>
+      </plmg-text-input>
+      `;
 
-      let htmlContent = '';
-      errors.forEach((errorControl) => {
-        htmlContent += `
-    <plmg-text-input label='error ${errorControl}' error-message='${errorControl}'>
-    </plmg-text-input>
-    `;
-      });
+    await page.setContent('<main>' + htmlContent + '</main>');
 
-      await page.setContent('<main>' + htmlContent + '</main>');
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
 
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
-
-      expect(results.violations).toHaveLength(0);
-    });
+    expect(results.violations).toHaveLength(0);
   });
 
-  describe('label visibilties', () => {
-    it('are accessible', async () => {
-      const page = await newE2EPage();
+  it('label visibilties are accessible', async () => {
+    const page = await newE2EPage();
 
-      const showLabels = [true, false];
+    const showLabels = [true, false];
 
-      let htmlContent = '';
-      showLabels.forEach((showLabel) => {
-        htmlContent += `
-    <plmg-text-input label='show-label ${showLabel}' show-label='${showLabel}'>
-    </plmg-text-input>
-    `;
-      });
-
-      await page.setContent('<main>' + htmlContent + '</main>');
-
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
-
-      expect(results.violations).toHaveLength(0);
+    let htmlContent = '';
+    showLabels.forEach((showLabel) => {
+      htmlContent += `
+  <plmg-text-input label='show-label ${showLabel}' show-label=${showLabel}>
+  </plmg-text-input>
+  `;
     });
+
+    await page.setContent('<main>' + htmlContent + '</main>');
+
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
+
+    expect(results.violations).toHaveLength(0);
   });
 
-  describe('tip text', () => {
-    it('are accessible', async () => {
-      const page = await newE2EPage();
+  it('tip text is accessible', async () => {
+    const page = await newE2EPage();
 
-      const tips = ['tip', undefined];
+    const htmlContent = `
+      <plmg-text-input label='tip' tip-text='tip text'>
+      </plmg-text-input>
+      `;
 
-      let htmlContent = '';
-      tips.forEach((tipTextControl) => {
-        htmlContent += `
-    <plmg-text-input label='tip ${tipTextControl}' tip-text='tip text'>
-    </plmg-text-input>
-    `;
-      });
+    await page.setContent('<main>' + htmlContent + '</main>');
 
-      await page.setContent('<main>' + htmlContent + '</main>');
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
 
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
-
-      expect(results.violations).toHaveLength(0);
-    });
+    expect(results.violations).toHaveLength(0);
   });
 
-  describe('required', () => {
-    it('is accessible', async () => {
-      const page = await newE2EPage();
+  it('required is accessible', async () => {
+    const page = await newE2EPage();
 
-      const required = [true, false];
-      let htmlContent = '';
-      required.forEach((requiredControl) => {
-        htmlContent += `
-    <plmg-text-input label='required ${requiredControl}' required=${requiredControl}>
-    </plmg-text-input>
-    `;
-      });
-
-      await page.setContent('<main>' + htmlContent + '</main>');
-
-      const results = await new AxePuppeteer(page as unknown as Page)
-        .disableRules([
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-        ])
-        .analyze();
-
-      expect(results.violations).toHaveLength(0);
+    const required = [true, false];
+    let htmlContent = '';
+    required.forEach((requiredControl) => {
+      htmlContent += `
+  <plmg-text-input label='required ${requiredControl}' required=${requiredControl}>
+  </plmg-text-input>
+  `;
     });
+
+    await page.setContent('<main>' + htmlContent + '</main>');
+
+    const results = await new AxePuppeteer(page as unknown as Page)
+      .disableRules([
+        'document-title',
+        'html-has-lang',
+        'landmark-one-main',
+        'page-has-heading-one',
+      ])
+      .analyze();
+
+    expect(results.violations).toHaveLength(0);
   });
 });
