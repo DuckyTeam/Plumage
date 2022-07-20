b6cefa0e322d35150f20bc963a50e60493296081
nathan-bird <nathan2484@hotmail.co.uk>
2022-07-20 05:41:14 +0200

    feat(storybook):Add detailed stories to showcase component

--- packages/component-library/src/components/plmg-tabs/plmg-tabs.stories.js
+++ packages/component-library/src/components/plmg-tabs/plmg-tabs.stories.js
@@ -7,26 +7,76 @@ export default {
   argTypes: {},
 };
 
-const PROPS = ['color'];
-const EVENTS = [];
-const CSS_VARS = [];
-const SLOTS = ['text'];
+export const Tabs = () => {
+  const htmlContent = `
+  <plmg-tabs>
+    <plmg-tab active label="Tab 1"></plmg-tab>
+    <plmg-tab label="Tab 2"></plmg-tab>
+    <plmg-tab label="Tab 3"></plmg-tab>
+  </plmg-tabs>
+  <br/>
+  `;
 
-const Template = (args) => {
-  const el = document.createElement('plmg-tabs');
-  Utils.bindProps(el, PROPS, args);
-  Utils.bindEvents(el, EVENTS, args);
-  Utils.bindStyles(el, CSS_VARS, args);
-  Utils.bindSlots(el, SLOTS, args);
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent.trim();
   return el;
 };
+Tabs.storyName = 'Tabs';
 
-export const Primary = Template.bind({});
-Primary.storyName = 'neutral';
-Primary.args = {
-  text: 'Tabs',
-  color: 'neutral',
+export const Icons = () => {
+  const htmlContent = `
+  <plmg-tabs>
+    <plmg-tab active icon="home" label="Tab 1"></plmg-tab>
+    <plmg-tab icon="accountCircle"></plmg-tab>
+  </plmg-tabs>
+  <br/>
+  `;
+
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent.trim();
+  return el;
 };
+Icons.storyName = 'Icon Tabs';
+
+export const Disabled = () => {
+  const htmlContent = `
+  <plmg-tabs>
+    <plmg-tab disabled label="Tab 1"></plmg-tab>
+  </plmg-tabs>
+  <br/>
+  `;
+
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent.trim();
+  return el;
+};
+Disabled.storyName = 'Disabled Tab';
+
+export const Scrolling = () => {
+  const htmlContent = `
+  <plmg-tabs>
+    <plmg-tab active label="Tab One"></plmg-tab>
+    <plmg-tab label="Tab Two"></plmg-tab>
+    <plmg-tab label="Tab Three"></plmg-tab>
+    <plmg-tab label="Tab Four"></plmg-tab>
+    <plmg-tab label="Tab Five"></plmg-tab>
+    <plmg-tab label="Tab Six"></plmg-tab>
+    <plmg-tab label="Tab Seven"></plmg-tab>
+    <plmg-tab label="Tab Eight"></plmg-tab>
+    <plmg-tab label="Tab Nine"></plmg-tab>
+    <plmg-tab label="Tab Ten"></plmg-tab>
+    <plmg-tab label="Tab Eleven"></plmg-tab>
+    <plmg-tab label="Tab Twelve"></plmg-tab>
+    <plmg-tab label="etc."></plmg-tab>
+  </plmg-tabs>
+  <br/>
+  `;
+
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent.trim();
+  return el;
+};
+Scrolling.storyName = 'Scrollable Tabs';
 
 export const All = () => {
   const htmlContent = `
