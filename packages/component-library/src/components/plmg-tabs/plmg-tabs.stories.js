134a8a839a03a13be5f1e20ed43dcbffa3d18137
nathan-bird <nathan2484@hotmail.co.uk>
2022-07-28 13:17:28 +0200

    refactor(stories):Remove scrollable tabs story

--- packages/component-library/src/components/plmg-tabs/plmg-tabs.stories.js
+++ packages/component-library/src/components/plmg-tabs/plmg-tabs.stories.js
@@ -50,32 +50,6 @@ export const Disabled = () => {
 };
 Disabled.storyName = 'Disabled Tab';
 
-export const Scrolling = () => {
-  const htmlContent = `
-  <plmg-tabs>
-    <plmg-tab active label="Tab One"></plmg-tab>
-    <plmg-tab label="Tab Two"></plmg-tab>
-    <plmg-tab label="Tab Three"></plmg-tab>
-    <plmg-tab label="Tab Four"></plmg-tab>
-    <plmg-tab label="Tab Five"></plmg-tab>
-    <plmg-tab label="Tab Six"></plmg-tab>
-    <plmg-tab label="Tab Seven"></plmg-tab>
-    <plmg-tab label="Tab Eight"></plmg-tab>
-    <plmg-tab label="Tab Nine"></plmg-tab>
-    <plmg-tab label="Tab Ten"></plmg-tab>
-    <plmg-tab label="Tab Eleven"></plmg-tab>
-    <plmg-tab label="Tab Twelve"></plmg-tab>
-    <plmg-tab label="etc."></plmg-tab>
-  </plmg-tabs>
-  <br/>
-  `;
-
-  const el = document.createElement('div');
-  el.innerHTML = htmlContent.trim();
-  return el;
-};
-Scrolling.storyName = 'Scrollable Tabs';
-
 export const All = () => {
   const htmlContent = `
   <plmg-tabs>
