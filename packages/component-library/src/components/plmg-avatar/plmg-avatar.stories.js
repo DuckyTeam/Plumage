8ada98e0ad21f30c776620355cd457bd3a88222b
jamesducky <james@ducky.eco>
2022-08-02 12:31:33 +0200

    fix(story):add all variations

--- packages/component-library/src/components/plmg-avatar/plmg-avatar.stories.js
+++ packages/component-library/src/components/plmg-avatar/plmg-avatar.stories.js
@@ -10,6 +10,12 @@ export default {
       options: ['small', 'medium', 'large', 'extra-large'],
       control: { type: 'select' },
     },
+    label: {
+      control: { type: 'text' },
+    },
+    interactive: {
+      control: { type: 'boolean' },
+    },
     ['image-url']: {
       control: { type: 'text' },
     },
@@ -19,7 +25,7 @@ export default {
   },
 };
 
-const PROPS = ['size', 'image-url', 'user-deleted'];
+const PROPS = ['size', 'image-url', 'interactive', 'label', 'user-deleted'];
 
 const Template = (args) => {
   const el = document.createElement('plmg-avatar');
@@ -32,7 +38,9 @@ Primary.storyName = 'Avatar';
 Primary.args = {
   size: 'medium',
   ['image-url']: 'https://static.ducky.eco/icons/maskable_icon_192.png',
+  interactive: true,
   ['user-deleted']: false,
+  label: 'user avatar',
 };
 
 export const UserDeleted = Template.bind({});
@@ -57,6 +65,37 @@ export const AllSizes = (args) => {
   const el = document.createElement('div');
   el.innerHTML = htmlContent;
   el.style.display = 'flex';
+  el.style.justifyContent = 'space-between';
+
+  return el;
+};
+AllSizes.storyName = 'All Sizes';
+
+export const AllVariations = (args) => {
+  const userDeleted = [true, false];
+  const imageUrl = [
+    undefined,
+    'https://static.ducky.eco/icons/maskable_icon_192.png',
+  ];
+
+  let htmlContent = '';
+  imageUrl.forEach((image) => {
+    userDeleted.forEach((deleted) => {
+      sizes.forEach((size) => {
+        if (image && !deleted) {
+          htmlContent += `<plmg-avatar image-url=${image} size=${size}></plmg-avatar>`;
+        } else if (image || !deleted) {
+          htmlContent += `<plmg-avatar user-deleted=${deleted} size=${size}></plmg-avatar>`;
+        }
+      });
+    });
+  });
+
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent;
+  el.style.display = 'grid';
+  el.style.gridTemplateColumns = 'repeat(4, 1fr)';
+  el.style.gap = '20px';
   el.style.justifyContent = 'space-between';
 
   return el;
