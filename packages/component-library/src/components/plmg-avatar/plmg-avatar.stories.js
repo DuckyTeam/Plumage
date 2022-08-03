c6c159beca0406a195bee060baba83521a54c144
jamesducky <james@ducky.eco>
2022-08-03 15:08:03 +0200

    fix(story): add colors, interactive

--- packages/component-library/src/components/plmg-avatar/plmg-avatar.stories.js
+++ packages/component-library/src/components/plmg-avatar/plmg-avatar.stories.js
@@ -6,6 +6,12 @@ export default {
   parameters: {},
   decorators: [],
   argTypes: {
+    ['background-color']: {
+      control: { type: 'color' },
+    },
+    ['icon-color']: {
+      control: { type: 'color' },
+    },
     size: {
       options: ['small', 'medium', 'large', 'extra-large'],
       control: { type: 'select' },
@@ -25,7 +31,15 @@ export default {
   },
 };
 
-const PROPS = ['size', 'image-url', 'interactive', 'label', 'user-deleted'];
+const PROPS = [
+  'background-color',
+  'icon-color',
+  'size',
+  'image-url',
+  'interactive',
+  'label',
+  'user-deleted',
+];
 
 const Template = (args) => {
   const el = document.createElement('plmg-avatar');
@@ -56,6 +70,34 @@ NoImage.args = {
   size: 'medium',
 };
 
+export const Color = (args) => {
+  const backgroundColors = [
+    'var(--plmg-color-background-info)',
+    'var(--plmg-color-background-success)',
+  ];
+  const iconColors = [
+    'var(--plmg-color-text-info-strong)',
+    'var(--plmg-color-text-neutral)',
+  ];
+
+  let htmlContent =
+    '<p>Default</p><plmg-avatar interactive size="medium" icon="home"></plmg-avatar><p>Examples</p>';
+
+  backgroundColors.forEach((backgroundColor) => {
+    iconColors.forEach((iconColor) => {
+      htmlContent += `<plmg-avatar interactive size="medium" icon="home" background-color=${backgroundColor} icon-color=${iconColor}></plmg-avatar>`;
+    });
+  });
+
+  const el = document.createElement('div');
+  el.innerHTML = htmlContent;
+  el.style.display = 'flex';
+  el.style.justifyContent = 'space-between';
+  el.style['flex-wrap'] = 'wrap';
+  return el;
+};
+Color.storyName = 'Color';
+
 export const AllSizes = (args) => {
   const htmlContent = sizes
     .map((size) => `<plmg-avatar size=${size}></plmg-avatar>`)
@@ -72,42 +114,19 @@ export const AllSizes = (args) => {
 AllSizes.storyName = 'All Sizes';
 
 export const AllVariations = (args) => {
-  const userDeleted = [false, true];
-  const imageUrl = [
-    'https://static.ducky.eco/icons/maskable_icon_192.png',
-    undefined,
-  ];
-
   let htmlContent = '';
-  htmlContent += `<p>Interactive</p><span></span><span></span><span></span>`;
-  imageUrl.forEach((image) => {
-    userDeleted.forEach((deleted) => {
-      sizes.forEach((size) => {
-        if (image !== undefined) {
-          htmlContent += `<plmg-avatar interactive user-deleted=${deleted} image-url=${image} size=${size}></plmg-avatar>`;
-        } else if (!deleted) {
-          htmlContent += `<plmg-avatar interactive user-deleted=${deleted} size=${size}></plmg-avatar>`;
-        }
-      });
-    });
+  sizes.forEach((size) => {
+    htmlContent += `<p>Non interactive ${size}</p><plmg-avatar image-url='https://static.ducky.eco/icons/maskable_icon_192.png' size=${size}></plmg-avatar><plmg-avatar size=${size}></plmg-avatar><plmg-avatar user-deleted size=${size}></plmg-avatar>`;
   });
-  htmlContent += `<p>Not Interactive</p><span></span><span></span><span></span>`;
-  imageUrl.forEach((image) => {
-    userDeleted.forEach((deleted) => {
-      sizes.forEach((size) => {
-        if (image !== undefined) {
-          htmlContent += `<plmg-avatar user-deleted=${deleted} image-url=${image} size=${size}></plmg-avatar>`;
-        } else if (!deleted) {
-          htmlContent += `<plmg-avatar user-deleted=${deleted} size=${size}></plmg-avatar>`;
-        }
-      });
-    });
+  sizes.forEach((size) => {
+    htmlContent += `<p>Interactive ${size}</p><plmg-avatar interactive image-url='https://static.ducky.eco/icons/maskable_icon_192.png' size=${size}></plmg-avatar><plmg-avatar interactive size=${size}></plmg-avatar><plmg-avatar interactive user-deleted size=${size}></plmg-avatar>`;
   });
 
   const el = document.createElement('div');
   el.innerHTML = htmlContent;
   el.style.display = 'grid';
   el.style.gridTemplateColumns = 'repeat(4, 1fr)';
+  el.style.fontSize = '14px';
   el.style.gap = '20px';
   el.style.justifyContent = 'space-between';
 
