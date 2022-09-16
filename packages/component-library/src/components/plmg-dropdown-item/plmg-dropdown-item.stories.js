5ff28b8b5e707e36ec9292432b8443e14ad0a8ec
jamesducky <104433221+jamesducky@users.noreply.github.com>
2022-09-16 16:52:38 +0200

    feat(stories): add component stories

---
+++ packages/component-library/src/components/plmg-dropdown-item/plmg-dropdown-item.stories.js
@@ -0,0 +1,66 @@
+import * as Utils from '../../stories/StencilStorybookUtils';
+import { ICON } from '../plmg-svg-icon/icon.enum';
+
+export default {
+  title: 'Component/DropdownItem',
+  parameters: {},
+  decorators: [],
+  argTypes: {
+    href: {
+      control: { type: 'text' },
+    },
+    icon: {
+      options: [undefined, ...Object.values(ICON)],
+      control: { type: 'select' },
+    },
+    rel: {
+      control: { type: 'text' },
+    },
+    target: {
+      control: { type: 'text' },
+    },
+    text: {
+      control: { type: 'text' },
+    },
+  },
+};
+
+const PROPS = ['href', 'icon', 'rel', 'target', 'text'];
+const EVENTS = [];
+const CSS_VARS = [];
+const SLOTS = [''];
+
+const Template = (args) => {
+  const el = document.createElement('plmg-dropdown-item');
+  Utils.bindProps(el, PROPS, args);
+  Utils.bindEvents(el, EVENTS, args);
+  Utils.bindStyles(el, CSS_VARS, args);
+  Utils.bindSlots(el, SLOTS, args);
+  return el;
+};
+
+export const Primary = Template.bind({});
+Primary.storyName = 'DropdownItem';
+Primary.args = {
+  text: 'A very long text that will be truncated',
+  href: 'https://www.ducky.eco/',
+  icon: 'duck',
+  href: 'https://ducky.eco',
+  rel: 'noopener noreferrer',
+  target: '_blank',
+};
+
+export const Link = Template.bind({});
+Link.storyName = 'Link';
+Link.args = {
+  text: 'Visit Ducky',
+  icon: 'duck',
+  href: 'https://www.ducky.eco/',
+  icon: 'duck',
+  href: 'https://ducky.eco',
+};
+export const Button = Template.bind({});
+Button.storyName = 'Button';
+Button.args = {
+  text: 'Button',
+};
---
+++ packages/component-library/src/components/plmg-dropdown/plmg-dropdown.stories.js
@@ -0,0 +1,71 @@
+import * as Utils from '../../stories/StencilStorybookUtils';
+
+export default {
+  title: 'Component/Dropdown',
+  parameters: {},
+  decorators: [],
+  argTypes: {
+    ['trigger']: { control: { type: 'text' } },
+    ['menu']: { control: { type: 'text' } },
+    ['disable-listeners']: {
+      options: [true, false],
+      control: { type: 'boolean' },
+    },
+    align: {
+      options: ['left', 'right'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+const PROPS = ['align', 'disable-listeners'];
+const EVENTS = [];
+const SLOTS = ['trigger', 'menu'];
+
+const Template = (args) => {
+  const el = document.createElement('plmg-dropdown');
+  Utils.bindProps(el, PROPS, args);
+  Utils.bindEvents(el, EVENTS, args);
+  Utils.bindSlots(el, SLOTS, args);
+  return el;
+};
+
+export const Primary = Template.bind({});
+Primary.storyName = 'Dropdown';
+Primary.args = {
+  align: 'left',
+  ['disable-listeners']: false,
+  ['trigger']: `<plmg-avatar style="width: 48px" slot="trigger" size="medium"></plmg-avatar>`,
+  ['menu']: `<div slot="menu">
+  <plmg-dropdown-item icon="duck" text="Dropdown Item 1" href="https://ducky.eco" target="_blank"></plmg-dropdown-item>
+  <plmg-dropdown-item text="Dropdown Item 2" href="https://plumage.ducky.eco/" target="_blank"></plmg-dropdown-item>
+  <plmg-separator style="padding: 8px 8px; color: #BFCBD1;"></plmg-separator>
+  <plmg-dropdown-item text="Dropdown Item 3">Item</plmg-dropdown-item>
+  </div>`,
+};
+
+export const AllAlignments = (args) => {
+  let htmlContent = ``;
+  htmlContent += `
+  <div style="display: flex; justify-content: space-between; padding: 16px;">
+  <plmg-dropdown align="left">
+  <plmg-status slot="trigger" variant="info">Left</plmg-status>
+  <div slot="menu">
+  <plmg-dropdown-item text="Link" href="https://ducky.eco" target="_blank"></plmg-dropdown-item>
+  <plmg-separator style="padding: 8px 8px; color: #BFCBD1;"></plmg-separator>
+  <plmg-dropdown-item text="Button">Item</plmg-dropdown-item>
+  </div>
+  </plmg-dropdown>
+    <plmg-dropdown align="right">
+    <plmg-status slot="trigger" variant="info">Right</plmg-status>
+    <div slot="menu">
+    <plmg-dropdown-item text="Link" href="https://ducky.eco" target="_blank"></plmg-dropdown-item>
+    <plmg-separator style="padding: 8px 8px; color: #BFCBD1;"></plmg-separator>
+    <plmg-dropdown-item text="Button">Item</plmg-dropdown-item>
+    </div>
+    </plmg-dropdown>
+  </div>`;
+
+  return htmlContent;
+};
+AllAlignments.storyName = 'AllAlignments';
