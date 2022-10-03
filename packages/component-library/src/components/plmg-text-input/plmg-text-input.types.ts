38f284f429f92a77fafe6b93cc379d17ee1fc3be
jamesducky <104433221+jamesducky@users.noreply.github.com>
2022-10-03 08:36:26 +0200

    feat(component):adds native input element attributes and emit validity object

--- packages/component-library/src/components/plmg-text-input/plmg-text-input.types.ts
+++ packages/component-library/src/components/plmg-text-input/plmg-text-input.types.ts
@@ -3,3 +3,17 @@ export type PlmgTextInputSize = typeof sizes[number];
 export function isPlmgTextInputSize(x: any): x is PlmgTextInputSize {
   return sizes.includes(x);
 }
+
+export const types = [
+  'text',
+  'email',
+  'password',
+  'tel',
+  'url',
+  'search',
+  'number',
+] as const;
+export type PlmgTextInputType = typeof types[number];
+export function isPlmgTextInputType(x: any): x is PlmgTextInputType {
+  return types.includes(x);
+}
