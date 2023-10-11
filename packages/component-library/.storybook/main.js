a5236a8cb8ca457aaf22f5feedf70063045370e4
jamesmarriott <james@ducky.eco>
2023-10-11 06:32:08 +0200

    chore(packages):migrate storybook

--- packages/component-library/.storybook/main.js
+++ packages/component-library/.storybook/main.js
@@ -1,7 +1,21 @@
+import { dirname, join } from 'path';
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5',
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+  ],
+
+  framework: {
+    name: getAbsolutePath('@storybook/web-components-webpack5'),
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
   },
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
